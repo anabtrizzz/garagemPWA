ef855ccb85cef4b37eb2384197137a9ee97a1afd
anabtrizzz <anabtriz.oliveira@gmail.com>
2026-04-09 19:36:28 -0300

    Remove 'Débito' option from Cliente Tipo

--- testeGaragem/Models/Clientes.cs
+++ testeGaragem/Models/Clientes.cs
@@ -6,7 +6,7 @@
         public string Nome { get; set; } = string.Empty;
         public string Telefone { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
-        public string Tipo { get; set; } = "Padrão"; // Padrão, Premium, Débito
+        public string Tipo { get; set; } = "Padrão"; // Padrão, Premium
         public DateTime DataCadastro { get; set; } = DateTime.Now;
     }
 }
